ba44590e41c291cf2a4fbc955fb62db8eb1d7f6b
LinukaAr <linukaarambawela@gmail.com>
2023-08-21 17:47:53 +0530

    Session Config

---
+++ src/util/SessionFactoryConfiguration.java
@@ -0,0 +1,32 @@
+package util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import entity.StudentEntity;
+
+public class SessionFactoryConfiguration {
+
+    private static SessionFactoryConfiguration sessionFactoryConfiguration;
+
+    private SessionFactory sessionFactory;
+
+    private SessionFactoryConfiguration() {
+        Configuration configuration = new Configuration().configure()
+        .addAnnotatedClass(StudentEntity.class);
+
+        sessionFactory = configuration.buildSessionFactory();
+    }
+
+    public static SessionFactoryConfiguration getInstance() {
+        return sessionFactoryConfiguration == null ? 
+                sessionFactoryConfiguration = new SessionFactoryConfiguration()
+                : sessionFactoryConfiguration;
+    }
+
+    public Session getSession(){
+        return sessionFactory.openSession();
+    }
+
+}
